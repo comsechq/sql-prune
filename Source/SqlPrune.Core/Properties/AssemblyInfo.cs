779ccc1840edb9eebefdc1160775028ce47e91b1
bounav <b.fourmond@gmail.com>
2016-06-06 11:22:09 +0100

    Bumped version to 0.3.2.1

--- Source/SqlPrune.Core/Properties/AssemblyInfo.cs
+++ Source/SqlPrune.Core/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Comsec Investigations Ltd")]
 [assembly: AssemblyProduct("SqlPrune.Core")]
-[assembly: AssemblyCopyright("Copyright © Comsec Solutions 2014")]
+[assembly: AssemblyCopyright("Copyright © Comsec Investigations 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.2.0")]
-[assembly: AssemblyFileVersion("0.3.2.0")]
+[assembly: AssemblyVersion("0.3.2.1")]
+[assembly: AssemblyFileVersion("0.3.2.1")]
